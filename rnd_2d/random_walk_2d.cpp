b78bac1febc2e7b82c56415f06f564fb9bc9803e
Slackware Live User <live@darkstar.example.net>
2019-12-09 15:44:24 -0800

    Algoritmo random walk 2d

--- rnd_2d/random_walk_2d.cpp
+++ rnd_2d/random_walk_2d.cpp
@@ -1,4 +1,7 @@
+//Programa que realiza el camino aleatorio en 2d
+
 #include <random>
+#include <algorithm>
 #include <iostream>
 #include <cstdio>
 
@@ -6,59 +9,110 @@
  
 int main()
 {
-  int N=500;//Repeticiones
-  
-   int Steps=100;
-   
-   double x=0.0;
+  //creo variables que me dan la cantidad de caminos que realzo (N) y la cantidad de pasos por camino (Steps)
+  int N=5000;//Repeticiones
+  int Steps=1000;
 
-  int *X= new int[N+1];
-  int *Avg= new int[Steps+1]; //Valor promedio de la posicion
+  int pasos=Steps;//Variable utilizada para  parar el programa si el camino aleatorio se encuentra con un punto sin salida posible.
+  int dim=2;//Dimension del programa
+  int contador=0;//Cuenta los pasos que se van realizando
+  int max_vec=dim*2; //Cantidad maxima de vecinos que puede tener cada punto
+  double random=0.0;
+  int x1=0,x2=0;//Variables temporales donde guardo los cambios de las respectivas coordenads
+
+  // int part[max_vec];//sectores que se divide la unidad
+  // int X1[Steps*N];
+  // int X2[Steps*N];
+  //int Avg[Steps]; //Valor promedio de la posicion
+
+  int *part= new int[max_vec];//sectores que se divide la unidad
+  int *X1= new int[Steps*N];
+  int *X2= new int[Steps*N];
+  int *Avg= new int[Steps]; //Valor promedio de la posicion
   
-   for(int i=0;i<=N;++i)
+   
+  // int *X= new int[N+1];
+  // int *Y= new int[N+1];
+  // int *Avg= new int[Steps+1]; //Valor promedio de la posicion
+  // for(int i=0;i<=Steps;++i)
+ 
+
+  ///////////////////////////////////////////////////////////////////////////////////////
+  //Inicializo los arreglos que se van a utilizar
+  
+  for(int i=1;i<=max_vec;++i)
+    part[i-1]=i*1.0;
+  
+   for(int i=0;i<Steps*N;++i)
      {
-       X[i]=0;
+       X1[i]=0;  X2[i]=0;
      }
 
-   for(int i=0;i<=Steps;++i)
+   for(int i=0;i<Steps;++i)
+     Avg[i]=0;
+     
+   //////////////////////////////////////////////////////////////////////////////////////
+   //Creo algoritmo que permite generar numeros aleatorios
+   
+   std::random_device rd;  //Will be used to obtain a seed for the random number engine
+   //  int seed=8;
+   //  std::mt19937 gen(seed);
+   std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+   std::uniform_real_distribution<> dis(0.0,  1.0);
+
+   //////////////////////////////////////////////////////////////////////////////////////
+   // Inicio funcion que calcula el camino aleatorio en dos dimensions
+   
+   for(int i=0;i<N;++i)// bucle que realiza diferentes caminos
      {
-       Avg[i]=0;
+       for(int j=0;j<Steps-1;++j)// Bucle que da cada paso aleatorio
+	 {
+	   random=dis(gen); // Se genera numero aleatorio 
+
+	   if(random<part[0]*1.0/max_vec){//Algoritmo que desice entre las posibilidades a moverse en funcion del numero random
+	     x1+=1;
+	   }
+	   else if(random<part[1]*1.0/max_vec ){
+	     x1-=1;
+	   }
+	   else if(random<part[2]*1.0/max_vec){
+	     x2+=1;
+	   }
+	   else if(random<part[3]*1.0/max_vec){
+	     x2-=1;
+	   }
+	   //Actualizo las posiciones y las guardo en dos arreglos 
+
+	   X1[j+1+i*Steps]=x1+X1[j+i*Steps];
+	   X2[j+1+i*Steps]=x2+X2[j+i*Steps];
+	   x1=0;
+	   x2=0;
+	   max_vec=dim*2;
+	 }
      }
+
+   //Se imprime cada punto del camino realzado y su respectivo calor r^2
    
-    std::random_device rd;  //Will be used to obtain a seed for the random number engine
-    std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
-    std::uniform_real_distribution<> dis(0.0,  1.0);
-
-   
-for (int n = 1; n <=Steps; ++n) {
-  printf("%5d",n);
-
-   for(int i=1;i<=N;++i)
-   {
-      if(dis(gen)>0.5){
-	X[i]+=1;
-	      }
-      else{
-	X[i]-=1;
-      }
-      
-      printf(" %5d ",X[i]);
-	
-      //std::cout << dis(gen) << ' ';
-      Avg[n]+=X[i]*X[i];
-   }
-  x=Avg[n]*1.0/N;
-
-  printf(" %5.1f ",x);
-
-  std::cout << '\n';
+   for(int i=0;i<pasos;++i){
+     for(int j=i;j<Steps*N;j+=Steps){
+       printf(" %4d %4d ",X1[j],X2[j]);
+       Avg[i]+=X1[j]*X1[j]+X2[j]*X2[j];
+     }
+     printf("%7.3f %5d \n",Avg[i]*1.0/N,contador);
+     contador++;
  }
 // std::cout << '\n';
 
-     delete [] X;
-     delete [] Avg;
+     // delete [] X;
+     // delete [] Y;
+     // delete [] Avg;
+
+   delete [] part;//sectores que se divide la unidad
+   delete [] X1;
+   delete [] X2;
+   delete [] Avg; //Valor promedio de la posicion
+ // printf(" %d ",pasos);
+   
+ return 0;
 }
 
-// double desplzamiento(){
-//   return 0;
-// }
--- rnd_2d/run.sh
+++ rnd_2d/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pwd
 name="random_walk_2d"
-VAR=0.1
+VAR=0.05
 Name_walk="Walk"
 
 g++ -std=c++17 -Wall -g -fsanitize=address -fsanitize=leak -fsanitize=undefined  $name.cpp -o $name.x
@@ -14,8 +14,8 @@ cat <<EOF> $name.gp
 
 set term pdf
 set output "Rand_walk_vs_steps.pdf"
-set xlabel "Steps"
-set ylabel "x"
+set xlabel "x"
+set ylabel "y"
 set grid
 #set xtics 50
 #set ytics 250
@@ -25,8 +25,8 @@ set grid
 set key left top box
 #unset key
 plot [][] "$name.txt" u 1:2 title "$Name_walk 1" w lp ps $VAR lt rgb "red" pointtype 7, \
-     	  "$name.txt" u 1:3  title "$Name_walk 2" w lp ps $VAR lt rgb "blue" pointtype 7, \
-	  "$name.txt" u 1:5  title "$Name_walk 3" w lp ps $VAR lt rgb "green" pointtype 7
+     	 # "$name.txt" u 3:4  title "$Name_walk 2" w lp ps $VAR lt rgb "blue" pointtype 7, \
+	  #"$name.txt" u 5:6  title "$Name_walk 3" w lp ps $VAR lt rgb "green" pointtype 7
 
 set term pdf
 set output "Mean_Rand_walk_vs_steps.pdf"
@@ -39,7 +39,7 @@ set grid
 #set mytics 2
 #set logscale x 2
 set key right bottom box
-plot [][] "$name.txt" u 1:502 w lp ps $VAR lt rgb "red" pointtype 7
+plot [][] "$name.txt" u 10002:10001 w lp ps $VAR lt rgb "red" pointtype 7
 replot x w lp ps $VAR lt rgb "blue" pointtype 7
 
 
