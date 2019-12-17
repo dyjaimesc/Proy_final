7bb56175ab30d6b81c9b06448fb68182e79a4340
Slackware Live User <live@darkstar.example.net>
2019-12-16 23:52:59 -0800

    Version final SAW 4 dimensiones

--- avoid_rnd_4d/avoid_rnd_4d.cpp
+++ avoid_rnd_4d/avoid_rnd_4d.cpp
@@ -1,72 +1,134 @@
-//Programa que realiza el self avoiding walk en 3d
+//Programa que realiza el self avoiding walk en 4d
+
+//////////////////////////////////////////
+/// Directivas del preprocesador
 
 #include <random>
-#include <algorithm>
 #include <iostream>
 #include <cstdio>
-#include<fstream>
+#include <cmath>
+#include <fstream>
+
+/////////////////////////////////////////
+
+//Se crean variables que  dan la cantidad de caminos que se realzan (N_walk) y la cantidad de pasos por camino (Steps)
+
+const int N_walk=200;//Numero de Caminos distintos e independientes
+const int Steps=50;//Pasos por camino
+const int dim=4;//Dimension del programa (4)
+int max_vec=dim*2; //Cantidad maxima de vecinos que puede tener cada punto
+
+//////////////////////////////////////////////////
+// Prototipos funciones
+
+void saw(int *X1,int *X2,int *X3,int *X4);//Funcion que realiza el camino aleatorio; parametros de entrada cada dimension (arreglo dinamico)
+
+void mean_StdDv(int *X1,int *X2,int *X3,int *X4,double *Avg,double *Std_Dev); //Funcion para encontrar la mediael promedio y la desviacion estandar de cada posicion 
 
 int lectura();//Funcion que lee la dimension del bloque
-// double desplazamiento();
- 
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+// main
+
 int main()
 {
-  //Se crean variables que  dan la cantidad de caminos que se realzan (N) y la cantidad de pasos por camino (Steps)
-  int N=200;//Repeticiones
-  int Steps=50;
 
-  int pasos=Steps;//Variable utilizada para  parar el programa si el camino aleatorio se encuentra con un punto sin salida posible.
-  int dim=4;//Dimension del programa
-  int contador=0;//Cuenta los pasos que se van realizando
-  int max_vec=dim*2; //Cantidad maxima de vecinos que puede tener cada punto
-  double random=0.0;
-  int x1=0,x2=0,x3=0,x4=0;//Variables temporales donde guardo los cambios de las respectivas coordenads
-
-  // int part[max_vec];//sectores que se divide la unidad
-  //int X1[Steps*N];
-  //  int X2[Steps*N];
-  //  int X3[Steps*N];
-  //  int Avg[Steps]; //Valor promedio de la posicion
-
-  int *part= new int[max_vec];//sectores que se divide la unidad
-  int *X1= new int[Steps*N];
-  int *X2= new int[Steps*N];
-  int *X3= new int[Steps*N];
-  int *X4= new int[Steps*N];
-  int *Avg= new int[Steps]; //Valor promedio de la posicion
+  // int X1[Steps*N_walk];
+  // int X2[Steps*N_walk];
+  // int X3[Steps*N_walk];
+  // int Avg[Steps]; //Valor promedio de la posicion
   
-  ///////////////////////////////////////////////////////////////////////////////////////
+  int *X1= new int[Steps*N_walk];
+  int *X2= new int[Steps*N_walk];
+  int *X3= new int[Steps*N_walk];
+  int *X4= new int[Steps*N_walk];
+
+  double *Avg= new double[Steps]; //Valor promedio de la posicion
+  double *Std_Dev= new double[Steps];//Desviacion estandar de cada posicion promediada
+
+  int contador=0;//Cuenta los pasos que se van realizando
+
+ ///////////////////////////////////////////////////////////////////////////////////////
   //Inicializo los arreglos que se van a utilizar
   
-  for(int i=1;i<=max_vec;++i)
-    part[i-1]=i*1.0;
-  
-   for(int i=0;i<Steps*N;++i)
+    for(int i=0;i<Steps*N_walk;++i)
      {
        X1[i]=0;  X2[i]=0; X3[i]=0; X4[i]=0;
      }
 
+    for(int i=0;i<Steps;++i)
+      {
+	Avg[i]=0.0; Std_Dev[i]=0.0;
+      }
+    ////////////////////////////////////////////////////////////////////////////////////////
+
+    saw(X1,X2,X3,X4); /// Se genera el los caminos aleatorios almacenando cada dimension en los arreglos X1, X2 y X3 
+
+    mean_StdDv(X1,X2,X3,X4,Avg,Std_Dev);
+    
+   //Se imprime cada punto del camino realzado y su respectivo calor r^2
+
+    
    for(int i=0;i<Steps;++i)
-     Avg[i]=0;
-     
-   //////////////////////////////////////////////////////////////////////////////////////
+     {
+       printf(" %5d %7.5f %7.5f",contador,Avg[i]*1.0,Std_Dev[i]);//N_walk);
+       contador++;
+
+       for(int j=i;j<Steps*N_walk;j+=Steps)
+	 printf(" %4d %4d %4d %4d ",X1[j],X2[j],X3[j],X4[j]);
+
+       printf("\n");
+	 
+     }
+
+ 
+   delete [] X1;
+   delete [] X2;
+   delete [] X3;
+   delete [] X4;
+   delete [] Avg;
+   delete [] Std_Dev;
+   
+ return 0;
+}
+
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+/////////////////////////////////////////////////////////////////
+
+void saw(int *X1,int *X2,int *X3,int *X4)
+{
+  
+  double random=0.0;
+  int x1=0,x2=0,x3=0,x4=0;//Variables temporales donde guardo los cambios de las respectivas coordenads
+  int part[max_vec];//sectores que se divide la unidad
+ 
+  for(int i=1;i<=max_vec;++i){
+    part[i-1]=i*1.0;
+  }
+   
+  ///////////////////////////////////////////////////////////
    //Algoritmo que permite generar numeros aleatorios
    
-   // std::random_device rd;  //Will be used to obtain a seed for the random number engine
-   int seed=lectura();
-      std::mt19937 gen(seed);
-      // std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+   //  std::random_device rd;  //Will be used to obtain a seed for the random number engine
+    
+    int seed=lectura();
+     std::mt19937 gen(seed);
+     //std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
    std::uniform_real_distribution<> dis(0.0,  1.0);
 
    //////////////////////////////////////////////////////////////////////////////////////
-   // Inicio funcion que calcula el camino aleatorio en tres dimensions
+   // Inicio funcion que calcula el camino aleatorio en tres dimensiones
    
-   for(int i=0;i<N;++i)// bucle que realiza diferentes caminos
+   for(int i=0;i<N_walk;++i)// bucle que realiza diferentes caminos
      {
        for(int j=0;j<Steps-1;++j)// Bucle que da cada paso aleatorio
 	 {
 	   random=dis(gen); // Se genera numero aleatorio
-	   
+
 	   ////////////////////////////////////////////////////////////////
  
 	   if(random<part[0]*1.0/max_vec){//Algoritmo que desice entre las posibilidades a moverse en funcion del numero random
@@ -103,54 +165,56 @@ int main()
 	   x2=0;
 	   x3=0;
 	   x4=0;
-	   max_vec=dim*2;
-
+	   
 	   ///////////////////////////////////////////////////////////////////
 	   // Algoritmo que verifica puntos recorridos
 
 	   for(int k=0;k<j;k++)
 	     {
-	       if(X1[j+i*Steps]==X1[k+i*Steps] && X2[j+i*Steps]==X2[k+i*Steps] && X3[j+i*Steps]==X3[k+i*Steps] && X4[j+i*Steps]==X4[k+i*Steps])//Reviso si la posicion j-esima de X2 ya esta ocupada en otra posicion k-esima
+	       if(X1[j+i*Steps]==X1[k+i*Steps] && X2[j+i*Steps]==X2[k+i*Steps] && X3[j+i*Steps]==X3[k+i*Steps] && X4[j+i*Steps]==X4[k+i*Steps])//Reviso si la posicion j-esima de X ya esta ocupada en otra posicion k-esima de los puntos ya recorridos
 		 {
-		   j=0;//Reinicio el camino.
+		   j=0;
 		   break;
 		 }
 	     }
-
-
-	   
 	 }
      }
-
-   //Se imprime cada punto del camino realzado y su respectivo calor r^2
-   
-   for(int i=0;i<pasos;++i)
-     {
-       for(int j=i;j<Steps*N;j+=Steps)
-	 {
-	   // printf(" %4d %4d %4d ",X1[j],X2[j],X3[j]);
-	   Avg[i]+=X1[j]*X1[j]+X2[j]*X2[j]+X3[j]*X3[j]+X4[j]*X4[j];
-	 }
-       printf(" %5d %7.3f ",contador,Avg[i]*1.0/N);
-       contador++;
-
-       for(int j=i;j<Steps*N;j+=Steps)
-	 printf(" %4d %4d %4d %4d",X1[j],X2[j],X3[j],X4[j]);
-
-       printf("\n");
-	 
-     }
-
-   delete [] part;//sectores que se divide la unidad
-   delete [] X1;
-   delete [] X2;
-   delete [] X3;
-   delete [] X4;
-   delete [] Avg; //Valor promedio de la posicion
- 
-   
- return 0;
+   //delete [] part;//sectores que se divide la unidad
 }
+
+/////////////////////////////////////////////////////////////////////
+
+
+void mean_StdDv(int *X1,int *X2,int *X3,int *X4,double *Avg,double *Std_Dev)
+{
+  double *Temp= new double[N_walk];//Variable temporal para guardar los r^2_i
+  int kk=0;
+    
+   for(int i=0;i<Steps;++i)
+     {
+       for(int j=i;j<Steps*N_walk;j+=Steps)
+	 {
+	   Temp[kk]=(X1[j]*X1[j]+X2[j]*X2[j]+X3[j]*X3[j]+X4[j]*X4[j]);//Acumulo r^2 en cada camino
+	   Avg[i]+=Temp[kk];
+	   kk++;
+	 }
+       Avg[i]=Avg[i]*1.0/(N_walk);
+       kk=0;
+
+       for(int k=0;k<N_walk;++k)
+	 {
+	   Std_Dev[i]+=Temp[k]*Temp[k]+Avg[i]*Avg[i]-2.0*Temp[k]*Avg[i];
+	 }
+       Std_Dev[i]=sqrt(Std_Dev[i]*1.0/(N_walk-1.0));
+             
+     }
+
+  delete [] Temp;
+  
+}
+
+//////////////////////////////////////////////////////////////
+////// Funcion utilizada para dar leer el valor que sele da a la semilla desde afuera
 
 int lectura(){
   int x=0;//Variable para guardar  el numero del bloque que viene dado del un archivo de texto externo creado en el script
@@ -176,3 +240,4 @@ int lectura(){
   archivo.close();
   return x;
 }
+
--- avoid_rnd_4d/run.sh
+++ avoid_rnd_4d/run.sh
@@ -1,75 +1,70 @@
 #!/bin/bash
 pwd
 name="avoid_rnd_4d"
+slope="Pendiente"
 VAR=0.05
 Name_walk="Walk"
+rm $slope.txt
 
-for i in {1..12}
+for i in {1..15}
 do
     rm -r entrada.dat
     echo $((i))>>entrada.dat # introduzco valor del bloque
 
     g++ -std=c++17 -Wall -g -fsanitize=address -fsanitize=leak -fsanitize=undefined  -O3 $name.cpp -o $name.x
-    #/usr/bin/time ./$name.x > $name.txt
-    ./$name.x > $name.txt
+    /usr/bin/time ./$name.x > $name.txt
+    #./$name.x > $name.txt
 
+    
 
-
-    cat <<EOF> $name.gp
+cat <<EOF> $name.gp
 
 
 
 set term pdf
-set output "Mean_Rand_walk_vs_steps.pdf"
+set output "Mean_SAW_vs_steps_4d.pdf"
 set xlabel "Steps"
-set ylabel "<x^2>"
+set ylabel "<r^2>"
 set grid
-#set xtics 50
-#set ytics 250
-#set mxtics 2
-#set mytics 2
-#set logscale x 
-#set logscale y
 set key right bottom box
 g(x)=x
-plot [][] "$name.txt" u 1:2 w lp ps $VAR lt rgb "red" pointtype 7, g(x) w lp ps $VAR lt rgb "blue" pointtype 7
+plot [][] "$name.txt" u 1:2:3 title "Datos SAW 4d" w p ps 0.5 lt rgb "red" pointtype 7 , g(x) title "Camino aleatorio libre" w lp ps $VAR lt rgb "blue" pointtype 7
 
 
 #f(x) = m*x + b
 #fit f(x) "$name.txt" using (log(\$1)):(log(\$2)) via m,b
 
 set term pdf
-set output "ajuste.pdf
+set output "ajuste_SAW_4d.pdf
 set xlabel "log(Steps)"
-set ylabel "log(<x^2>)"
+set ylabel "log(<r^2>)"
+set key left top box
 set fit quiet #Evita imprimir ajuste en consola
 f(x) = a*x + b
 a=1
 b=1
 fit f(x) "$name.txt" using (log(\$1)):(log(\$2)) via a,b
 #set fit logfile '/dev/null' #no crear un archivo fit.log
-print "pendiente of a is: ", a
-#set label "a=%6.2f", a, "+/- %6.2f", a_err
-save fit "datos_ajuste.txt
-plot "$name.txt" using (log(\$1)):(log(\$2)), f(x) title sprintf("y=m*x+b, m=%.4f; b=%.4f", a, b)
-
+set print "$slope.txt" append #append bandera para que me sobrescriba en el archivo ya existente
+print a #"$slope.txt" a
+save fit "datos_ajuste.txt"
+plot "$name.txt" using (log(\$1)):(log(\$2)):(log(\$3)) title "Datos" w p ps 0.5 lt rgb "red" pointtype 7 , f(x) title sprintf("Fit y=m*x+b, m=%.3f; b=%.3f", a, b)
 
 EOF
 gnuplot $name.gp
 
-#okular Rand_walk_vs_steps.pdf &
-#okular Mean_Rand_walk_vs_steps.pdf &
-#okular ajuste.pdf &
 done
 
-#for i in {1..12}
-#do
-    # echo $((N))
-  #  rm -r entrada.dat
- #   echo $((i))>>entrada.dat # introduzco valor del bloque
-   
-  #  N=$((i*2))
-  #  rm -r entrada.dat
-#done
 
-#kate entrada.dat &
+okular Mean_SAW_vs_steps_4d.pdf &
+okular ajuste_SAW_4d.pdf &
+
+name2="media_destv"
+
+g++ -std=c++17 -Wall -g -fsanitize=address -fsanitize=leak -fsanitize=undefined  -O3 $name2.cpp -o $name2.x
+#/usr/bin/time ./$name.x #> $name.txt
+echo -e "\n SAW en 4 dimensiones"
+./$name2.x #> $name.txt
+
+
+
